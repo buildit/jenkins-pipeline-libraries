bf6c17fa6913ac363be253edf8de4a53a5940898
dermot <dermotmburke@gmail.com>
2016-06-24 14:34:45 +0100

    Add ui

---
+++ lib/ui.groovy
@@ -0,0 +1,16 @@
+def dropdown(name, description, closure) {
+    def list = closure()
+
+    if (!list) {
+        return [$class: 'ChoiceParameterDefinition', choices: "", description: description, name: name]
+    }
+
+    String choices = "\n"
+    for (int i = 0; i < (list.size() as Integer); i++) {
+        choices = "${choices}${list[i]}\n"
+    }
+
+    return [$class: 'ChoiceParameterDefinition', choices: choices, description: description, name: name]
+}
+
+return this
