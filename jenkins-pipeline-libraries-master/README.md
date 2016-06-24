6a3fd697a93f2686ffd499579806a49c746c4517
dermot <dermotmburke@gmail.com>
2016-06-24 11:31:15 +0100

    Delete folder added in error

--- jenkins-pipeline-libraries-master/README.md
+++
@@ -1,3 +0,0 @@
-# Jenkins Pipeline Libraries
-
-Useful Jenkins Pipeline Libraries to use for whatever.
--- jenkins-pipeline-libraries-master/lib/utilities.groovy
+++
@@ -1,6 +0,0 @@
-
-def timestamp(Date date = new Date()){
-    return date.format('yyyyMMddHHmmss',TimeZone.getTimeZone('GMT')) as String
-}
-
-return this
