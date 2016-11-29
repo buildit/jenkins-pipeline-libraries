dba46ad1cfcc271d2f4c91dd045acfbb610a6dd5
rsafronov <electroma@gmail.com>
2016-11-28 19:58:12 -0500

    Template is NonCPS

--- src/template.groovy
+++ src/template.groovy
@@ -1,5 +1,7 @@
 import groovy.text.GStringTemplateEngine
+import com.cloudbees.groovy.cps.NonCPS
 
+@NonCPS
 def transform(String template, Map map) {
     new GStringTemplateEngine().createTemplate(template).make(map)
 }
