9f18a10e472128220ceac795a88b338ba8ee300c
Chris Bowers <christopher.bowers@wipro.com>
2017-03-10 16:19:13 +0000

    Try a symlink.

--- src/tools.groovy
+++ src/tools.groovy
@@ -17,14 +17,15 @@ def configureAndroid(String name='android', String pathToExecutable='tools', Str
     def path = tool name
 //    env.PATH = "${env.PATH}:${path}:${path}/tools:${path}/platform-tools"
 
-    androidHome = path.substring(0, path.lastIndexOf("/"))
-
 //    env.PATH = "${env.PATH}:${path}"
-    env.ANDROID_HOME = androidHome
+    env.ANDROID_HOME = path
     echo("Configured ANDROID_HOME: ${env.ANDROID_HOME}")
 //    echo("PATH is now ${env.PATH}")
 
     sh("(while sleep 4; do echo \"y\"; done) | ${path}/bin/sdkmanager ${sdkOpts}")
+
+    // Android looks for the licenses in the wrong directory, so we symlink it.
+    sh("ln -s ${path}/licenses ${path}/tools/licenses")
 //    sh("echo 'y' | ${path}/${pathToExecutable}/sdkmanager ${sdkOpts}")
 //    sh("echo 'y' | android update sdk --no-ui --filter 1,2")
 }
