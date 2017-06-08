f736204a379b2ba9e5bd4a2c464845ad9d1cf714
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-06-08 09:31:43 -0500

    cleanup and wait a little longer...some things take a while

--- src/convox.groovy
+++ src/convox.groovy
@@ -19,7 +19,7 @@ def deploy(appName, description) {
 }
 
 def waitUntilDeployed(appName) {
-    for (int i=0; i < 10; i++) {
+    for (int i=0; i < 50; i++) {
         def status = getShell().pipe("convox apps info --app ${appName} | grep Status | sed 's/Status *\\(.*\\)/\\1/'").trim()
         echo "${appName} is ${status}"
         if (status == "running") return
@@ -32,13 +32,13 @@ def ensureParameterSet(appName, parameter, value) {
     def currentValue = getShell().pipe("convox apps params --app ${appName} | grep ${parameter} | sed 's/${parameter} *\\(.*\\)/\\1/'").trim()
 
     if (currentValue != value) {
+        waitUntilDeployed(appName)
         sh "convox apps params set ${parameter}=${value} --app ${appName}"
         waitUntilDeployed(appName)
     }
 }
 
 def ensureSecurityGroupSet(appName, securityGroup) {
-
     if (!securityGroup.startsWith('sg-')) {
         error "Ensure you use the AWS Group ID for the security group"
         return
@@ -50,8 +50,9 @@ def ensureSecurityGroupSet(appName, securityGroup) {
 def ensureCertificateSet(appName, process, port, certificate) {
     def currentCert = getShell().pipe("convox ssl --app ${appName} | grep ${process}:${port} | cut -d ' ' -f3").trim()
     if (currentCert != certificate) {
+        waitUntilDeployed(appName)
         sh "convox ssl update ${process}:${port} ${certificate} --app ${appName}"
-        waitUntilDeployed("${appName}")
+        waitUntilDeployed(appName)
     }
 }
 
