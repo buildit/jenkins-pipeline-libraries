1a3628de5a361fc6c36b9ba75ff2f390a4c960c7
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-06-30 11:11:51 -0500

    try some global variables (for declarative pipelines)

---
+++ vars/slackNotify.groovy
@@ -0,0 +1,24 @@
+import groovy.json.JsonOutput
+
+def call(title, text, color, icon, channel) {
+    def slackURL = 'https://hooks.slack.com/services/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'
+
+    def payloadJson = [
+        channel: channel,
+        username: "Jenkins Overlord",
+        icon_url: icon,
+        attachments: [[
+            fallback: text,
+            color: color,
+            fields: [
+                [
+                    title: title,
+                    value: text,
+                    short: false
+                ]
+            ]
+        ]]
+    ]
+    def encodedReq = URLEncoder.encode(JsonOutput.toJson(payloadJson), "UTF-8")
+    sh "curl -X POST --data \'payload=${encodedReq}\' ${slackURL}"
+}
