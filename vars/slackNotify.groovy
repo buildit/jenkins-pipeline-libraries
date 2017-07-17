c8b68bbc8437811c64e8e8981ea863b86ab7295a
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-07-17 14:08:57 -0500

    abandon this approach for now

--- vars/slackNotify.groovy
+++
@@ -1,5 +0,0 @@
-def call(String title, String text, String color, String icon, String channel) {
-    echo "slackNotify start"
-    def slackInst = new slack();
-    slackInst.notify(title, text, color, icon, channel)
-}
