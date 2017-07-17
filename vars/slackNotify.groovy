13455c32d82db8b04ec93a1ff536564ac4e30f51
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-07-17 13:00:34 -0500

    test

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,6 +1,7 @@
 import groovy.json.JsonOutput
 
 def call(title, text, color, icon, channel) {
+    sh "echo slackNotify start"
     def slackURL = 'https://hooks.slack.com/services/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'
 
     def payloadJson = [
@@ -20,5 +21,6 @@ def call(title, text, color, icon, channel) {
         ]]
     ]
     def encodedReq = URLEncoder.encode(JsonOutput.toJson(payloadJson), "UTF-8")
+    sh "echo curl -X POST --data \'payload=${encodedReq}\' ${slackURL}"
     sh "curl -X POST --data \'payload=${encodedReq}\' ${slackURL}"
 }
